526a7826b8460b20787e4d3024552984980fc271
YUUKIToriyama <ytoriyama@torisfactory.com>
2019-12-03 12:55:54 +0900

    base64エンコード・デコードの日本語対応

--- main.js
+++ main.js
@@ -1,25 +1,46 @@
 /* main.js */
 
+// アドレスの末尾にbase64コードが付けられた時、それをデコードして実行する
 var code = location.search.slice(1);
-result = eval(atob(code));
+result = eval(base64Decode(code));
 
 if (result != undefined) {
 	alert(result);
 }
 
-/*
-var field = document.getElementById("field");
-field.innerHTML = "<h1>" + result + "</h1>";
-alert(result);
-*/
 
-var button = document.getElementById("btn");
-button.addEventListener("click", function() {
-	var encodedStr = base64Encode(document.form1.js_source.value);
-	//alert(encodedStr);
-	document.form1.bs64_output.value = encodedStr;
-});
+// textareaでtabを入力できるようにするコード
+document.getElementById("source").addEventListener("keydown", function(e) {
+	var elm, end, start, value;
+	if (e.keyCode === 9) {
+		if (e.preventDefault) {
+			e.preventDefault();
+		}
+		elm = e.target;
+		start = elm.selectionStart;
+		end = elm.selectionEnd;
+		value = elm.value;
+		elm.value = "" + (value.substring(0, start)) + "\t" + (value.substring(end));
+		elm.selectionStart = elm.selectionEnd = start + 1;
+		return false;
+	}
+})
 
+// 各ボタンを押すと処理が実行される
+var encodeButton = document.getElementById("encode_btn");
+encodeButton.addEventListener("click", function() {document.textConverter.output.value = base64Encode(document.textConverter.source.value)});
+var decodeButton = document.getElementById("decode_btn");
+decodeButton.addEventListener("click", function() {document.textConverter.output.value = base64Decode(document.textConverter.source.value)});
+
+// base64とユニコード文字列の相互変換
 function base64Encode(input) {
-	return btoa(input.replace(/\r?\n/g, ""))
+	return btoa(unescape(encodeURIComponent(input.replace(/\r?\n/g, ""))));
+}
+function base64Decode(input) {
+	try {
+		return decodeURIComponent(escape(atob(input)));
+	} catch(error) {
+		console.log("入力された文字列に問題があります");
+		return "ERROR";
+	}
 }
