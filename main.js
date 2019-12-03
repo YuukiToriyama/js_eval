0a0c27843e0bffa0a8968b332b47c953d40ed80f
YUUKI Toriyama <43945424+YUUKIToriyama@users.noreply.github.com>
2019-12-03 11:25:00 +0900

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,7 @@ result = eval(atob(code));
 var field = document.getElementById("field");
 field.innerHTML = "<h1>" + result + "</h1>";
 alert(result);
+
+
+var button = document.getElementById("btn");
+button.addEventListener("click", function(){alert(document.form1.textbox1.value)});
