fe0f40b17731f143b2c266e8ae9cc175f399d03b
YUUKI Toriyama <43945424+YUUKIToriyama@users.noreply.github.com>
2019-12-03 10:23:53 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ var code = location.search.slice(1)
 result = eval(atob(code));
 
 var field = document.getElementById("field");
-field.innerHTML = "<h1>" + filed + "</h1>";
+field.innerHTML = "<h1>" + result + "</h1>";
 alert(result);
