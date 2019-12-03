fa5e96817295bdf2b977a8a56d1d024ea3233e8c
YUUKIToriyama <ytoriyama@torisfactory.com>
2019-12-03 11:49:43 +0900

    modify index.html main.js

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 /* main.js */
+/*
 var code = location.search.slice(1)
 result = eval(atob(code));
 
 var field = document.getElementById("field");
 field.innerHTML = "<h1>" + result + "</h1>";
 alert(result);
-
+*/
 
 var button = document.getElementById("btn");
-button.addEventListener("click", function(){alert(document.form1.textbox1.value)});
+button.addEventListener("click", function() {
+	var encodedStr = base64Encode(document.form1.textarea1.value);
+	//alert(encodedStr);
+	document.form1.textarea2.value = encodedStr;
+});
+
+function base64Encode(input) {
+	return btoa(input.replace(/\r?\n/g, ""))
+}
